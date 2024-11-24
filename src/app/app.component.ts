b2a4bb0d6f2a2f4d866aea81d18095056f33af6c
Miller <millershaw61@gmail.com>
2024-11-24 23:19:31 +0000

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardContainerComponent } from './card-container/card-container.component';
+import { LanguagesCardComponent } from './languages-card/languages-card.component';
+import { ServicesCardComponent } from './services-card/services-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CardContainerComponent,
+    LanguagesCardComponent,
+    ServicesCardComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'portfolio';
