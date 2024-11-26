f9830304ac7e05564e15a2df9297781c341a2ad3
Miller <millershaw61@gmail.com>
2024-11-26 17:19:39 +0000

    Fix: Module not found
    Comment out dev env logic, instead of leaving it included. Gitignored the env.dev.ts this caused the error.

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -3,14 +3,15 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Repository } from '../card-container/repositories.model';
 import { environment } from '../../environment/environment';
-import { dev_environment } from '../../environment/environment.dev';
+// import { dev_environment } from '../../environment/environment.dev';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GithubService {
   private apiUrl = 'https://api.github.com';
-  private token = environment.GH_TOKEN ?? dev_environment.GH_TOKEN;
+  private token = environment.GH_TOKEN;
+  // dev_environment.GH_TOKEN;
 
   constructor(private http: HttpClient) {}
 
