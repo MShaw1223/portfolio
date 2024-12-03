d9d038fb6a0b5bdfca64a0aed984096090993813
Miller <millershaw61@gmail.com>
2024-12-03 01:10:38 +0000

    Feat: Project count, using existing GH service
    Pulls repos from GH service and displays the count of repositories. Style changes also implemented hopefully better looking on mobile.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { CardContainerComponent } from './card-container/card-container.componen
 import { LanguagesCardComponent } from './languages-card/languages-card.component';
 import { ServicesCardComponent } from './services-card/services-card.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { GithubService } from './services/github.service';
+import { Repository } from './card-container/repositories.model';
 
 @Component({
   selector: 'app-root',
@@ -19,4 +21,17 @@ import { NavbarComponent } from './navbar/navbar.component';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  constructor(private ghs: GithubService) {}
+
+  repos: Repository[] = [];
+
+  ngOnInit(): void {
+    this.ghs.getUserRepos('MShaw1223').subscribe({
+      next: (data: Repository[]) => {
+        this.repos = data;
+      },
+      error: (error) => console.error('Error fetching data:', error),
+    });
+  }
 }
